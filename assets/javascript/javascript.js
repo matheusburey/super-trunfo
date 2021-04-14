4dd53bdd8cd3c21e4d197827db7f5e2c8ca2c90e
matheusburey <matheusbureidev@gmail.com>
2021-04-13 21:44:56 -0300

    fucMostraImage

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -62,3 +62,30 @@ var piccolo = {
   var cartas = [vegeta, goku, gohan, broly, piccolo, trunks];
   var cartaMaquina;
   var cartaJogador;
+
+function sortearCarta(){
+  var numCartaMaquina = parseInt(Math.random() * cartas.length)
+  cartaMaquina = cartas[numCartaMaquina]
+  console.log(cartaMaquina.nome)
+
+  var numCartaJogador = parseInt(Math.random() * cartas.length)
+  cartaJogador = cartas[numCartaJogador]
+  console.log(cartaJogador.nome)
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibeCartaJogador()
+}
+
+function exibeCartaJogador(){
+  var divcartaJogador = document.getElementById("carta-jogador")
+  divcartaJogador.style.backgroundImage = `url(${cartaJogador.img})`
+
+  divcartaJogador.innerHTML = `<div id="opcoes" class="carta-status">
+  <input type="button" name="atributo" value="${cartaJogador.atributos.ki}"><br>
+  <input type="button" name="atributo" value="${cartaJogador.atributos.tecnicas}"><br>
+  <input type="button" name="atributo" value="${cartaJogador.atributos.velocidade}"><br>
+  <input type="button" name="atributo" value="${cartaJogador.atributos.transformasoes}"><br></div>`
+
+}
