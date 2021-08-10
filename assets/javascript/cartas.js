13fa3ee9001806fb61f85f3340ed4c40664c935f
matheusburey <matheusbureidev@gmail.com>
2021-08-10 10:01:35 -0300

    refatoraçao de codigo e adiçao das imagens das cartas

---
+++ assets/javascript/cartas.js
@@ -0,0 +1,62 @@
+const objetoCartas = {
+  'Son goku': {
+    nome: "Son-goku",
+    atributos: {
+      ki: 8700,
+      tecnicas: 8500,
+      velocidade: 85,
+      transformacoes: 7
+    },
+    img:"/assets/imagens/son-goku.jpg"
+  },
+  vegeta: {
+    nome: "Vegeta",
+    atributos: {
+      ki: 8500,
+      tecnicas: 8200,
+      velocidade: 58,
+      transformacoes: 5
+    },
+    img:"/assets/imagens/vegeta.jpg"
+  },
+  gohan: {
+    nome: "Gohan",
+    atributos: {
+      ki: 7800,
+      tecnicas: 7100,
+      velocidade: 80,
+      transformacoes: 3
+    },
+    img:"/assets/imagens/gohan.jpg"
+  },
+  broly: {
+    nome: "Broly",
+    atributos: {
+      ki: 9000,
+      tecnicas: 7500,
+      velocidade: 78,
+      transformacoes: 3
+    },
+    img:"/assets/imagens/broly.jpg"
+  },
+  'mirai trunks': {
+    nome: "Mirai Trunks",
+    atributos: {
+      ki: 8100,
+      tecnicas: 8300,
+      velocidade: 77,
+      transformacoes: 3
+    },
+    img:"/assets/imagens/mirai-trunks.jpg"
+  },
+  piccolo: {
+    nome: "Piccolo",
+    atributos: {
+      ki: 7000,
+      tecnicas: 7200,
+      velocidade: 65,
+      transformacoes: 1
+    },
+    img:"/assets/imagens/piccolo.jpg"
+  }
+};
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,65 +1,4 @@
-var vegeta = {
-    nome: "Vegeta",
-    atributos: {
-      ki: 8500,
-      tecnicas: 8200,
-      velocidade: 58,
-      transformacoes: 5
-    },
-    img:"https://cdna.artstation.com/p/assets/images/images/019/993/052/large/eduardo-medeiros-prancheta-3.jpg?1565888013"
-}; 
-var goku = {
-    nome: "Son-goku",
-    atributos: {
-      ki: 8700,
-      tecnicas: 8500,
-      velocidade: 85,
-      transformacoes: 7
-    },
-    img:"https://cdnb.artstation.com/p/assets/images/images/019/993/047/large/eduardo-medeiros-prancheta-1.jpg?1565888009"
-}; 
-var gohan = {
-    nome: "Gohan",
-    atributos: {
-      ki: 7800,
-      tecnicas: 7100,
-      velocidade: 80,
-      transformacoes: 3
-    },
-    img:"https://cdna.artstation.com/p/assets/images/images/019/993/056/large/eduardo-medeiros-prancheta-5.jpg?1565888021"
-}; 
-var broly = {
-    nome: "Broly",
-    atributos: {
-      ki: 9000,
-      tecnicas: 7500,
-      velocidade: 78,
-      transformacoes: 3
-    },
-    img: "https://cdnb.artstation.com/p/assets/images/images/019/993/055/large/eduardo-medeiros-prancheta-4.jpg?1565888017"
-}; 
-var trunks = {
-    nome: "Mirai Trunks",
-    atributos: {
-        ki: 8100,
-        tecnicas: 8300,
-        velocidade: 77,
-        transformacoes: 3
-    },
-    img:"https://cdnb.artstation.com/p/assets/images/images/019/993/057/large/eduardo-medeiros-prancheta-6.jpg?1565888025"
-}; 
-var piccolo = {
-    nome: "Piccolo",
-    atributos: {
-      ki: 7000,
-      tecnicas: 7200,
-      velocidade: 65,
-      transformacoes: 1
-    },
-    img:"https://cdna.artstation.com/p/assets/images/images/019/993/046/large/eduardo-medeiros-prancheta-7.jpg?1565888007"
-};
-
-var cartas = [vegeta, goku, gohan, broly, piccolo, trunks];
+const nomeCartas = Object.keys(objetoCartas);
 var cartaMaquina;
 var cartaJogador;
 var pontosJogador = 0;
@@ -72,12 +11,14 @@ function mostraPlacar(){
   divPlacar.innerHTML = `<h2>Jogador ${pontosJogador} X ${pontosMaquina} Maquina</h2>`
 }
 function sortearCarta(){
-  var numCartaMaquina = parseInt(Math.random() * cartas.length)
-  cartaMaquina = cartas[numCartaMaquina]
+  var numCartaMaquina = parseInt(Math.random() * nomeCartas.length)
+  let indexObjetoMaquina = nomeCartas[numCartaMaquina]
+  cartaMaquina = objetoCartas[indexObjetoMaquina]
   console.log(cartaMaquina.nome)
 
-  var numCartaJogador = parseInt(Math.random() * cartas.length)
-  cartaJogador = cartas[numCartaJogador]
+  var numCartaJogador = parseInt(Math.random() * nomeCartas.length)
+  let indexObjetoJogador = nomeCartas[numCartaJogador];
+  cartaJogador = objetoCartas[indexObjetoJogador]
   console.log(cartaJogador.nome)
 
   document.getElementById("btnSortear").disabled = true;
