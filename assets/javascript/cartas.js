ac09df408bf650086e4285d2222595e90a0bda6c
matheusburey <matheusbureidev@gmail.com>
2021-08-19 08:45:04 -0300

    adiçao de novas cartas ao game

--- assets/javascript/cartas.js
+++ assets/javascript/cartas.js
@@ -1,33 +1,53 @@
 const objetoCartas = {
-  'Son goku': {
-    nome: "Son-goku",
+  android17: {
+    nome: "android 17",
     atributos: {
-      ki: 8700,
-      tecnicas: 8500,
-      velocidade: 85,
-      transformacoes: 7
-    },
-    img:"/assets/imagens/son-goku.jpg"
-  },
-  vegeta: {
-    nome: "Vegeta",
-    atributos: {
-      ki: 8500,
-      tecnicas: 8200,
-      velocidade: 58,
-      transformacoes: 5
-    },
-    img:"/assets/imagens/vegeta.jpg"
-  },
-  gohan: {
-    nome: "Gohan",
-    atributos: {
-      ki: 7800,
+      ki: 7300,
       tecnicas: 7100,
-      velocidade: 80,
-      transformacoes: 3
+      velocidade: 84,
+      transformacoes: 0
     },
-    img:"/assets/imagens/gohan.jpg"
+    img:"assets/imagens/android17.jpg"
+  },
+  android18: {
+    nome: "android 18",
+    atributos: {
+      ki: 7300,
+      tecnicas: 6500,
+      velocidade: 82,
+      transformacoes: 0
+    },
+    img:"assets/imagens/android18.jpg"
+  },
+  aniraza: {
+    nome: "aniraza",
+    atributos: {
+      ki: 9000,
+      tecnicas: 7000,
+      velocidade: 75,
+      transformacoes: 0
+    },
+    img:"assets/imagens/aniraza.jpg"
+  },
+  bardock: {
+    nome: "bardock",
+    atributos: {
+      ki: 8000,
+      tecnicas: 7000,
+      velocidade: 81,
+      transformacoes: 2
+    },
+    img:"assets/imagens/bardock.jpg"
+  },
+  botamo: {
+    nome: "botamo",
+    atributos: {
+      ki: 5000,
+      tecnicas: 5700,
+      velocidade: 55,
+      transformacoes: 0
+    },
+    img:"assets/imagens/botamo.jpg"
   },
   broly: {
     nome: "Broly",
@@ -37,7 +57,187 @@ const objetoCartas = {
       velocidade: 78,
       transformacoes: 3
     },
-    img:"/assets/imagens/broly.jpg"
+    img:"assets/imagens/broly.jpg"
+  },
+  caulifla: {
+    nome: "caulifla",
+    atributos: {
+      ki: 8100,
+      tecnicas: 8000,
+      velocidade: 74,
+      transformacoes: 2
+    },
+    img:"assets/imagens/caulifla.jpg"
+  },
+  cell: {
+    nome: "cell",
+    atributos: {
+      ki: 7400,
+      tecnicas: 6000,
+      velocidade: 66,
+      transformacoes: 3
+    },
+    img:"assets/imagens/cell.jpg"
+  },
+  dyspo: {
+    nome: "dyspo",
+    atributos: {
+      ki: 8000,
+      tecnicas: 7900,
+      velocidade: 100,
+      transformacoes: 0
+    },
+    img:"assets/imagens/dyspo.jpg"
+  },
+  freeza: {
+    nome: "freeza",
+    atributos: {
+      ki: 8200,
+      tecnicas: 8300,
+      velocidade: 77,
+      transformacoes: 5
+    },
+    img:"assets/imagens/freeza.jpg"
+  },
+  frost: {
+    nome: "frost",
+    atributos: {
+      ki: 7100,
+      tecnicas: 5900,
+      velocidade: 70,
+      transformacoes: 3
+    },
+    img:"assets/imagens/frost.jpg"
+  },
+  gogeta: {
+    nome: "gogeta",
+    atributos: {
+      ki: 9500,
+      tecnicas: 9600,
+      velocidade: 88,
+      transformacoes: 4
+    },
+    img:"assets/imagens/gogeta.jpg"
+  },
+  gohan: {
+    nome: "Gohan",
+    atributos: {
+      ki: 7800,
+      tecnicas: 7100,
+      velocidade: 80,
+      transformacoes: 3
+    },
+    img:"assets/imagens/gohan.jpg"
+  },
+  'goku-black': {
+    nome: "goku black",
+    atributos: {
+      ki: 9000,
+      tecnicas: 8600,
+      velocidade: 81,
+      transformacoes: 3
+    },
+    img:"/assets/imagens/goku-black.jpg"
+  },
+  gotenks: {
+    nome: "gotenks",
+    atributos: {
+      ki: 8000,
+      tecnicas: 8500,
+      velocidade: 72,
+      transformacoes: 2
+    },
+    img:"assets/imagens/gotenks.jpg"
+  },
+  hit: {
+    nome: "hit",
+    atributos: {
+      ki: 8200,
+      tecnicas: 8800,
+      velocidade: 95,
+      transformacoes: 0
+    },
+    img:"assets/imagens/hit.jpg"
+  },
+  jiren: {
+    nome: "jiren",
+    atributos: {
+      ki: 10000,
+      tecnicas: 9000,
+      velocidade: 92,
+      transformacoes: 0
+    },
+    img:"assets/imagens/jiren.jpg"
+  },
+  kale: {
+    nome: "kale",
+    atributos: {
+      ki: 8300,
+      tecnicas: 8000,
+      velocidade: 71,
+      transformacoes: 2
+    },
+    img:"assets/imagens/kale.jpg"
+  },
+  kefla: {
+    nome: "kefla",
+    atributos: {
+      ki: 9200,
+      tecnicas: 8400,
+      velocidade: 84,
+      transformacoes: 2
+    },
+    img:"assets/imagens/kefla.jpg"
+  },
+  kuririn: {
+    nome: "kuririn",
+    atributos: {
+      ki: 5500,
+      tecnicas: 6000,
+      velocidade: 68,
+      transformacoes: 0
+    },
+    img:"assets/imagens/kuririn.jpg"
+  },
+  kyabe: {
+    nome: "kyabe",
+    atributos: {
+      ki: 8000,
+      tecnicas: 7000,
+      velocidade: 75,
+      transformacoes: 2
+    },
+    img:"assets/imagens/kyabe.jpg"
+  },
+  magetta: {
+    nome: "magetta",
+    atributos: {
+      ki: 6600,
+      tecnicas: 5500,
+      velocidade: 50,
+      transformacoes: 0
+    },
+    img:"assets/imagens/magetta.jpg"
+  },
+  'majin-boo': {
+    nome: "majin boo",
+    atributos: {
+      ki: 8300,
+      tecnicas: 9000,
+      velocidade: 72,
+      transformacoes: 6
+    },
+    img:"assets/imagens/majin-boo.jpg"
+  },
+  'mestre-kame': {
+    nome: "mestre kame",
+    atributos: {
+      ki: 4000,
+      tecnicas: 9100,
+      velocidade: 58,
+      transformacoes: 1
+    },
+    img:"assets/imagens/mestre-kame.jpg"
   },
   'mirai trunks': {
     nome: "Mirai Trunks",
@@ -47,7 +247,7 @@ const objetoCartas = {
       velocidade: 77,
       transformacoes: 3
     },
-    img:"/assets/imagens/mirai-trunks.jpg"
+    img:"assets/imagens/mirai-trunks.jpg"
   },
   piccolo: {
     nome: "Piccolo",
@@ -57,6 +257,106 @@ const objetoCartas = {
       velocidade: 65,
       transformacoes: 1
     },
-    img:"/assets/imagens/piccolo.jpg"
+    img:"assets/imagens/piccolo.jpg"
+  },
+  pilina: {
+    nome: "pilina",
+    atributos: {
+      ki: 7500,
+      tecnicas: 7000,
+      velocidade: 62,
+      transformacoes: 0
+    },
+    img:"assets/imagens/pilina.jpg"
+  },
+  ribrianne: {
+    nome: "ribrianne",
+    atributos: {
+      ki: 6700,
+      tecnicas: 7000,
+      velocidade: 71,
+      transformacoes: 1
+    },
+    img:"assets/imagens/ribrianne.jpg"
+  },
+  saonel: {
+    nome: "saonel",
+    atributos: {
+      ki: 7400,
+      tecnicas: 7100,
+      velocidade: 65,
+      transformacoes: 0
+    },
+    img:"assets/imagens/saonel.jpg"
+  },
+  shenlong: {
+    nome: "shenlong",
+    atributos: {
+      ki: 100000,
+      tecnicas: 10000,
+      velocidade: 10000,
+      transformacoes: 10000
+    },
+    img:"assets/imagens/shenlong.jpg"
+  },
+  'Son-goku': {
+    nome: "Son goku",
+    atributos: {
+      ki: 8700,
+      tecnicas: 8500,
+      velocidade: 85,
+      transformacoes: 7
+    },
+    img:"assets/imagens/son-goku.jpg"
+  },
+  tenshinhan: {
+    nome: "tenshinhan",
+    atributos: {
+      ki: 6500,
+      tecnicas: 6400,
+      velocidade: 66,
+      transformacoes: 1
+    },
+    img:"assets/imagens/tenshinhan.jpg"
+  },
+  toppo: {
+    nome: "toppo",
+    atributos: {
+      ki: 9200,
+      tecnicas: 8100,
+      velocidade: 78,
+      transformacoes: 0
+    },
+    img:"assets/imagens/toppo.jpg"
+  },
+  vegeta: {
+    nome: "Vegeta",
+    atributos: {
+      ki: 8500,
+      tecnicas: 8200,
+      velocidade: 58,
+      transformacoes: 5
+    },
+    img:"assets/imagens/vegeta.jpg"
+  },
+  vegetto: {
+    nome: "vegetto",
+    atributos: {
+      ki: 9200,
+      tecnicas: 9500,
+      velocidade: 90,
+      transformacoes: 4
+    },
+    img:"assets/imagens/vegetto.jpg"
+  },
+  zamasu: {
+    nome: "zamasu",
+    atributos: {
+      ki: 9400,
+      tecnicas: 8600,
+      velocidade: 84,
+      transformacoes: 1
+    },
+    img:"assets/imagens/zamasu.jpg"
   }
 };
