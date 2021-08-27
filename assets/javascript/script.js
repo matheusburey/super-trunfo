4734813a4ad80a0ca7fdc4c34b81355066f45d2c
matheusburey <matheusbureidev@gmail.com>
2021-08-27 08:51:49 -0300

    Refatoração

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,75 @@
 const screen = document.getElementById('screen');
-const nameCards = Object.keys(objetoCartas);
 const objPlayers = {
   player: {
     cards: '',
-    points: 0
+    points: 0,
+    amountCards: 0
   },
   pc: {
     cards: '',
     points: 0
+  },
+  returnImgaen: function(player){
+    return this[player].cards.img;
+  },
+  returnAtributs: function(player, atribut) {
+    return this[player].cards.atributos[atribut];
   }
 };
+
+const initialOptions = () => {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  const select = document.createElement('select');
+  const options1 = document.createElement('option');
+  const options2 = document.createElement('option');
+  const options3 = document.createElement('option');
+  const button = document.createElement('button');
+
+  div.className = 'initiDiv';
+  span.innerText = 'Deck com:';
+  select.className = 'game-options';
+  select.id = 'numberCards'
+  options1.innerText = '03 cartas';
+  options1.value = '03';
+  options2.innerText = '05 cartas';
+  options2.value = '05';
+  options3.innerText = '10 cartas';
+  options3.value = '10';
+  button.innerText = 'Start Game';
+  button.id = 'startGame';
+  button.className = 'button';
+
+  select.appendChild(options1);
+  select.appendChild(options2);
+  select.appendChild(options3);
+  div.appendChild(span);
+  div.appendChild(select);
+  div.appendChild(button);
+  screen.appendChild(div);
+}
 
 const functionRoutes = (event) => {
   const clickedTarget = event.target;
   const clickedTargetId = clickedTarget.id;
   
   if (clickedTargetId === "startGame") {
-    startGame(clickedTarget);
+    amountOfCards(objPlayers);
+    cleanScreen();
+    startGame();
+    setWaitingTime(500, drawLetter, objetoCartas);
   };
   if(clickedTarget.name === "atributo"){
-    console.log(clickedTargetId);
     playerAttribute(clickedTargetId);
   }
 };
 
-const startGame = clickedTarget => {
+const amountOfCards = ({player}) => {
+  const number = document.getElementById('numberCards');
+  player.amountCards = Number(number.value);
+};
+
+const startGame = () => {
   const scoreboard = document.createElement('div');
   const scoreboardText = document.createElement('h2');
   const winner = document.createElement('di');
@@ -33,7 +78,6 @@ const startGame = clickedTarget => {
   const imgCardPc = document.createElement('img');
   const imgCardPlayer = document.createElement('img');
 
-  clickedTarget.parentElement.classList.add('hidden');
   scoreboardText.innerHTML = 'Jogador 0 X 0 Maquina';
   scoreboard.classList = 'scoreboard';
   winner.className = 'winner'
@@ -53,8 +97,6 @@ const startGame = clickedTarget => {
   screen.appendChild(scoreboard);
   screen.appendChild(cardPlayer);
   screen.appendChild(cardPc);
-
-  sortearCarta();
 };
 
 const showScore = () => {
@@ -62,22 +104,27 @@ const showScore = () => {
   scoreboard.innerText = `Jogador ${objPlayers.player.points} X ${objPlayers.pc.points} Maquina`
 };
 
-const sortearCarta = () => {
-  var numCartaMaquina = parseInt(Math.random() * nameCards.length)
-  let indexObjetoMaquina = nameCards[numCartaMaquina]
-  cartaMaquina = objetoCartas[indexObjetoMaquina]
-  console.log(cartaMaquina.nome)
-
-  var numCartaJogador = parseInt(Math.random() * nameCards.length)
-  let indexObjetoJogador = nameCards[numCartaJogador];
-  cardPlayer = objetoCartas[indexObjetoJogador]
-  console.log(cardPlayer.nome)
-
-  displaysPlayerCard();
+const random = (maxValue) => {
+  return parseInt(Math.random() * maxValue)
 }
 
+const returnLetters = (objetoCards) => {
+  const nameCards = Object.keys(objetoCards);
+  let index = random(nameCards.length);
+  let indexCard = nameCards[index];
+  return objetoCards[indexCard];
+};
+
+const drawLetter = (objetoCards) => {
+  objPlayers.pc.cards = returnLetters(objetoCards);
+
+  objPlayers.player.cards = returnLetters(objetoCards);
+
+  displaysPlayerCard();
+};
+
 function displaysPlayerCard() {
-  const attributesValue = ['ki', "tecnica", "velocidade", "transformacoes"];
+  const attributesValue = ['ki', "tecnicas", "velocidade", "transformacoes"];
   const divCardPlayer = document.querySelector("#card-player")
   const imgCardPlayer = document.querySelector("#card-player .cards");
   const divInputs = document.createElement('div');
@@ -90,15 +137,17 @@ function displaysPlayerCard() {
     divInputs.appendChild(input);
   }
 
-  imgCardPlayer.src = cardPlayer.img;
+  imgCardPlayer.src = objPlayers.returnImgaen('player');
   divInputs.classList = "cards-status";
   divCardPlayer.appendChild(divInputs);
 }
 
 function playerAttribute(selectedAttribute) {
+  console.log(selectedAttribute)
   const vencedor = document.querySelector(".winner")
-  const playerSelectedAttribute = cardPlayer.atributos[selectedAttribute];
-  const pcSelectedAttribute = cartaMaquina.atributos[selectedAttribute];
+  const playerSelectedAttribute = objPlayers.returnAtributs('player',selectedAttribute);
+  const pcSelectedAttribute = objPlayers.returnAtributs('pc',selectedAttribute);
+  console.log(playerSelectedAttribute,pcSelectedAttribute)
 
   if (playerSelectedAttribute > pcSelectedAttribute) {
     vencedor.innerHTML = "<h2>Venceu</h2>"
@@ -114,22 +163,46 @@ function playerAttribute(selectedAttribute) {
   exibeCartaMaquina();
 }
 
-function exibeCartaMaquina() {
+const exibeCartaMaquina = () => {
   const divCards = document.getElementsByClassName("cards");
-  divCards[1].src = cartaMaquina.img;
+  divCards[1].src = objPlayers.returnImgaen('pc');
 
-  setWaitingTime(4000,reset);
+  setWaitingTime(5000, divertLetter);
 }
 
-const setWaitingTime = (time, callBack) =>{
-  setTimeout(function () { callBack(); }, time);
-};
-
-const reset = () => {
+const divertLetter = () => {
   const divCards = document.getElementsByClassName("cards");
   divCards[0].src = 'assets/imagens/fundo.jpg';
   divCards[1].src = 'assets/imagens/fundo.jpg';
-  sortearCarta();
+
+  objPlayers.player.amountCards--;
+  if(objPlayers.player.amountCards > 0){
+    drawLetter(objetoCartas);
+  }else {
+    reset();
+  }
 }
 
+const setWaitingTime = (time, callBack, value) =>{
+  setTimeout(function () { callBack(value); }, time);
+};
+
+const reset = () => {
+  cleanScreen()
+
+  objPlayers.player.cards = []
+  objPlayers.player.index = 0
+  objPlayers.player.points = 0
+
+  objPlayers.pc.cards = []
+  objPlayers.pc.index = 0
+  objPlayers.pc.points = 0
+  initialOptions()
+}
+
+const cleanScreen = () => {
+  screen.innerText = ''
+}
+
+initialOptions();
 screen.addEventListener("click", functionRoutes);
